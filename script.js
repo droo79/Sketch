d15a35bf7061e4af2635b1e24f9686357dc96b7a
Dylan roo <dreuterpro@gmail.com>
2021-08-13 19:13:29 -0400

    index.html

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+var color = 'black';
 
+const container = document.querySelector('#container');
+gridSize(16);
+
+function gridSize(n) {
+    const grid = document.getElementById('container');
+    grid.style.gridTemplateColumns = `repeat(${n}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${n}, 1fr)`
+    let gridXY = n * n;
+
+    for (let i = 0; i < gridXY; i++) {
+        const rows = document.createElement('div');
+        rows.classList.add('rows');
+        rows.addEventListener('mouseenter', bgColor);
+        container.appendChild(rows);
+    }
+}
+
+function bgColor () {
+    if (color == 'black')
+    {
+        this.style.backgroundColor = 'black';
+    }
+    else if (color == 'rainbow')
+    {
+        let randomColor = Math.floor(Math.random()*16777215).toString(16);
+        this.style.backgroundColor = "#" + randomColor;
+    }
+}
+    
+const clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', clearColor);
+
+function clearColor () {
+    container.innerHTML = '';
+    var size = prompt("How many squares per side?");
+    gridSize(size);
+}
+
+const rainbowButton = document.getElementById('rainbow');
+rainbowButton.addEventListener('click', rainbowColor);
+
+function rainbowColor() {
+    color = 'rainbow';
+}
+      
